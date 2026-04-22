1227d2100f4b4097f14a0531d74cee5d1477a4a9
dsdhillon27 <123650351+dsdhillon27@users.noreply.github.com>
2026-04-22 07:22:59 +0530

    First commit

---
+++ src/main/java/com/dsd/resolveai/ResolveAiApplication.java
@@ -0,0 +1,13 @@
+package com.dsd.resolveai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolveAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResolveAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dsd/resolveai/ResolveAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsd.resolveai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResolveAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

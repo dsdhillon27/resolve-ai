41c118576e52387bd7089f8be1fed8e0dd89382f
dsdhillon27 <123650351+dsdhillon27@users.noreply.github.com>
2026-04-24 15:17:23 +0530

    Integrated RAG in ChatService

--- src/main/java/com/dsd/resolveai/service/ChatService.java
+++ src/main/java/com/dsd/resolveai/service/ChatService.java
@@ -1,6 +1,11 @@
 package com.dsd.resolveai.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,8 +13,13 @@ public class ChatService {
 
     private final ChatClient chatClient;
 
-    public ChatService(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
+    public ChatService(ChatClient.Builder chatClientBuilder, VectorStore vectorStore) {
+        this.chatClient = chatClientBuilder
+                .defaultAdvisors(
+                    QuestionAnswerAdvisor.builder(vectorStore).build(),
+                    new SimpleLoggerAdvisor()
+                )
+                .build();
     }
 
     public String chat(String message) {
